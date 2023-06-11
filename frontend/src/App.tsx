7885660b50caf4bbae1280f2cc2477816f4583f5
Meph384 <jakub.kozak384@gmail.com>
2023-06-11 20:32:42 +0200

    feat: Mockup of IngredientsList

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,15 @@
 import '@fontsource/roboto/400.css';
 import IngredientsSearch from "./IngredientsSearch";
+import IngredientsList from "./IngredientsList";
 
-function App(){
+function App() {
     return(
         <div className="container flex justify-center flex-col items-center mx-auto">
             <h1 className="text-3xl font-bold underline mb-2 mt-2">
                 IQchnia
             </h1>
             <IngredientsSearch/>
+            <IngredientsList />
         </div>
     );
 }
