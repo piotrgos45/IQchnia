a5409006faa826b1e9e9a85a1f71449cf3106466
bedziox <daniel.bednarek2001@gmail.com>
2023-06-19 09:17:34 +0200

    small design changes

--- frontend/src/components/RecipeElement.tsx
+++ frontend/src/components/RecipeElement.tsx
@@ -1,5 +1,6 @@
 
 import { Recipe } from "src/types"
+import { BiTime  } from "react-icons/bi"
 
 interface IRecipe{
     recipe: Recipe
@@ -7,8 +8,14 @@ interface IRecipe{
 
 const RecipeElement:React.FC<IRecipe> = ({recipe}) => {
     return (
-        <div className="borderbox border-2">
+        <div className="grid column-1 borderbox border-2 border-grey align-center items-center text-center rounded-lg border-slate-500">
             <h3>{recipe.name}</h3>
+            <span className="flex items-center justify-center"> <BiTime/> {recipe.cook_time / 60} min</span>
+            <span> 
+                {recipe.fat ? 'T:' + recipe.fat + ' ' : null} 
+                {recipe.carbs ? 'W:' + recipe.carbs + ' ' : null} 
+                {recipe.protein ? 'B:' + recipe.protein + ' '  : null}
+            </span> 
         </div>
     )
 }
